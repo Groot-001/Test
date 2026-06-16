33e2006237d4ebd6663a84a1067fe6e12a6b804e
Pratik Patel <pratiikpatel.001@gmail.com>
2026-06-16 20:05:22 +0545

    TanstackRouter Setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: { tsconfigPaths: true },
+  plugins: [
+    tanstackRouter({
+      target: "react",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
